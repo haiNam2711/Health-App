25fcaf0eb475e96fd334bdbd74ae0417162a9ebb
haiNam2711 <namcuong2711@gmail.com>
2023-12-01 00:25:35 +0700

    Finish developing IntroViewController

---
+++ HealthApp/U/Constant.swift
@@ -0,0 +1,26 @@
+//
+//  Constant.swift
+//  HealthApp
+//
+//  Created by Hai Nam on 30/11/2023.
+//
+
+import Foundation
+
+struct K {
+    struct IntroVCConstant {
+        static let CellNibName = "IntroCollectionViewCell"
+        static let CellId = "introCell"
+        static let PhotoArray = ["img-intro1", "img-intro2", "img-intro3"]
+    }
+    
+    struct Color {
+        static let darkGreen = "DarkGreen"
+        static let lightGreen = "LightGreen"
+    }
+    
+    struct Font {
+        static let NunitoBold = "NunitoSans-12ptExtraLight_Bold"
+        static let NunitoRegular = "NunitoSans-12ptExtraLight_Regular"
+    }
+}
