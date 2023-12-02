20b0987951841314ea9cb9ada7e0c9b32e600ccc
haiNam2711 <namcuong2711@gmail.com>
2023-12-02 09:02:14 +0700

    Cache Images for imagecell

--- HealthApp/Controller/IntroViewController.swift
+++ HealthApp/Controller/IntroViewController.swift
@@ -16,8 +16,7 @@ class IntroViewController: UIViewController {
     @IBOutlet private weak var bottomLbl: UILabel!
     @IBOutlet private var stepper: [UIView]!
     
-    private let photos = K.IntroVCConstant.PhotoArray
-
+    private var photos: [UIImage]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +28,7 @@ class IntroViewController: UIViewController {
         
         setUpButtonFont()
         
+        photos = imagePhotos(imageNames: K.IntroVCConstant.PhotoArray)
         let nib = UINib(nibName: K.IntroVCConstant.CellNibName, bundle: nil)
         introCollectionView.register(nib, forCellWithReuseIdentifier: K.IntroVCConstant.CellId)
 
@@ -47,6 +47,14 @@ class IntroViewController: UIViewController {
             stepper[i].backgroundColor = (stepper[i].tag == num) ? UIColor(named: K.Color.darkGreen) : UIColor(named: K.Color.lightGreen)
         }
     }
+    
+    func imagePhotos(imageNames: [String]) -> [UIImage] {
+        var res : [UIImage] = []
+        for str in imageNames {
+            res.append(UIImage(named: str)!)
+        }
+        return res
+    }
 
     
 }
@@ -54,12 +62,12 @@ class IntroViewController: UIViewController {
 extension IntroViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+        return photos!.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.IntroVCConstant.CellId, for: indexPath) as! IntroCollectionViewCell
-        cell.imageView.image = UIImage(named: photos[indexPath.row])
+        cell.imageView.image = photos![indexPath.row]
         return cell
     }
     
